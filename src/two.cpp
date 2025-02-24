95f3b49ca05439327e10359f8ad5688b7a0921d5
christophergyman <christophergayiuman@gmail.com>
2025-02-24 01:05:22 +0000

    feat: added code comments to two.cpp

--- src/two.cpp
+++ src/two.cpp
@@ -3,24 +3,31 @@
 #include <websocketpp/config/asio_no_tls.hpp>
 #include <websocketpp/server.hpp>
 
+// rename websocketpp to server
 typedef websocketpp::server<websocketpp::config::asio> server;
 
+// pass through currenet websocket connection, and message pointer
 void on_message(websocketpp::connection_hdl, server::message_ptr msg)
 {
+    // print out the message payload form ptr
     std::cout << msg->get_payload() << std::endl;
 }
 
 int main()
 {
+    // define print_server
     server print_server;
 
+    // on message call function on message
     print_server.set_message_handler(&on_message);
     print_server.set_access_channels(websocketpp::log::alevel::all);
     print_server.set_error_channels(websocketpp::log::elevel::all);
 
+    // init, listen, and accept packets from network
     print_server.init_asio();
     print_server.listen(9002);
     print_server.start_accept();
 
+    // run the server
     print_server.run();
 }
