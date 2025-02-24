7c1f015d3f22c29a26a89d862e480548b772936c
christophergyman <christophergayiuman@gmail.com>
2025-02-24 01:00:24 +0000

    feat: working c++ websocket

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,28 @@
+#define ASIO_STANDALONE
+#include <websocketpp/config/asio_no_tls.hpp>
+#include <websocketpp/server.hpp>
 #include <iostream>
+
+typedef websocketpp::server<websocketpp::config::asio> server;
 
 int main()
 {
-    std::cout << "Hello world! \n";
+    try
+    {
+        server ws_server;
+
+        // Set logging settings
+        ws_server.set_access_channels(websocketpp::log::alevel::all);
+        ws_server.clear_access_channels(websocketpp::log::alevel::frame_payload);
+
+        // Initialize ASIO
+        ws_server.init_asio();
+
+        std::cout << "WebSocket++ Server initialized" << std::endl;
+    }
+    catch (websocketpp::exception const &e)
+    {
+        std::cout << "Error: " << e.what() << std::endl;
+    }
     return 0;
 }
