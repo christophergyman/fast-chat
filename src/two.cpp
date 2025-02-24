fcfc814bacf751c889241f9f932e2a7a498fd98b
christophergyman <christophergayiuman@gmail.com>
2025-02-24 01:20:57 +0000

    feat: added server response

--- src/two.cpp
+++ src/two.cpp
@@ -3,31 +3,35 @@
 #include <websocketpp/config/asio_no_tls.hpp>
 #include <websocketpp/server.hpp>
 
-// rename websocketpp to server
 typedef websocketpp::server<websocketpp::config::asio> server;
 
-// pass through currenet websocket connection, and message pointer
-void on_message(websocketpp::connection_hdl, server::message_ptr msg)
+// Function to handle incoming messages
+void on_message(server *s, websocketpp::connection_hdl hdl, server::message_ptr msg)
 {
-    // print out the message payload form ptr
-    std::cout << msg->get_payload() << std::endl;
+    std::cout << "Received: " << msg->get_payload() << std::endl;
+
+    // Send the message back to the client
+    try
+    {
+        s->send(hdl, "Server received: " + msg->get_payload(), websocketpp::frame::opcode::text);
+    }
+    catch (websocketpp::exception const &e)
+    {
+        std::cout << "Send failed: " << e.what() << std::endl;
+    }
 }
 
 int main()
 {
-    // define print_server
     server print_server;
 
-    // on message call function on message
-    print_server.set_message_handler(&on_message);
+    print_server.set_message_handler(std::bind(&on_message, &print_server, std::placeholders::_1, std::placeholders::_2));
     print_server.set_access_channels(websocketpp::log::alevel::all);
     print_server.set_error_channels(websocketpp::log::elevel::all);
 
-    // init, listen, and accept packets from network
     print_server.init_asio();
-    print_server.listen(9002);
+    print_server.listen(9003);
     print_server.start_accept();
 
-    // run the server
     print_server.run();
 }
